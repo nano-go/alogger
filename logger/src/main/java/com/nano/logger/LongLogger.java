d9e87fb84f6ed92adb1f7c7768f62248407d4a89
nano-go <1522862112@qq.com>
2021-07-20 16:08:03 +0800

    details.

--- logger/src/main/java/com/nano/logger/LongLogger.java
+++ logger/src/main/java/com/nano/logger/LongLogger.java
@@ -2,23 +2,18 @@ package com.nano.logger;
 import com.nano.logger.config.LoggerConfiguration;
 
 /**
- * This class solves the problem that due to the limitation in tne Android System, 
- * the size of each printed log will be limited.
+ * This class solves the problem due to the size limitation of the log in
+ * the Android System.
  *
  * <pre>
- * if the size of each printed log is greater than (1024 * 4) bits, the log contents 
- * will be looped out.
+ * If the size of each log is greater than (1024 * 4) bits, the log will 
+ * be divided into multiple logs to print.
  * 
- * But for performance reasons, the length of each printed log is still less than 
- * {@link LoggerConfiguration#getLogMaxLength()} that the default value is 
- * {@link LoggerConfiguration#DEFAULT_LOG_MAX_LENGTH}. If you want to change it 
- * see @{link LoggerConfiguration#setLogMaxLength(int)}.
+ * But for performance reasons, the length of each log is still less
+ * than {@link LoggerConfiguration#getLogMaxLength()} that the default
+ * value is {@link LoggerConfiguration#DEFAULT_LOG_MAX_LENGTH}. If you
+ * want to change it, see @{link LoggerConfiguration#setLogMaxLength(int)}.
  * <pre>
- * 
- * <pre>
- * 		Author: nano1
- *		Date  : 20/5/17
- * </pre>
  */
 public class LongLogger extends ILogger {
 
